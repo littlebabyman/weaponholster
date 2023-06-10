a92b25e67d9915bc84ebf384b1fd6f061ef50007
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2023-06-10 14:56:22 +0300

    spiffy caching

--- lua/autorun/sh_holsterweapon.lua
+++ lua/autorun/sh_holsterweapon.lua
@@ -1,9 +1,10 @@
-CreateConVar("holsterweapon_ladders", 1, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Enable holstering your weapon on ladders.", 0, 2)
-CreateConVar("holsterweapon_undraw", 1, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Allow playing weapon draw animation backwards, as a fallback.", 0, 1)
-CreateConVar("holsterweapon_weapon", "", {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}, "Weapon to holster to. Invalid weapon returns default holster. Requires restart to change.")
-CreateClientConVar("holsterweapon_key", 18, true)
+local CVarFlags = {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED}
+local LadderCVar = CreateConVar("holsterweapon_ladders", 1, CVarFlags, "Enable holstering your weapon on ladders.", 0, 2)
+local UndrawCVar = CreateConVar("holsterweapon_undraw", 1, CVarFlags, "Allow playing weapon draw animation backwards, as a fallback.", 0, 1)
+local WeaponCVar = CreateConVar("holsterweapon_weapon", "", CVarFlags, "Weapon to holster to. Invalid weapon returns default holster. Requires restart to change.")
+local BindCVar = CreateClientConVar("holsterweapon_key", 18, true)
+local holster = WeaponCVar:GetString()
 
-local holster = GetConVar("holsterweapon_weapon"):GetString()
 timer.Simple(0, function()
     if engine.ActiveGamemode() == "terrortown" then
         holster = "weapon_ttt_unarmed"
@@ -31,15 +32,24 @@ if CLIENT then
         end)
     end)
 
+    local HLAZCvar = GetConVar("hlaz_sv_holster")
+    local SSCVar = GetConVar("ss_enableholsterdelay")
+
     function SimpleHolster()
         local ply = LocalPlayer()
         if !ply:Alive() || ply.Holstering then return end
         local weapon = ply:GetActiveWeapon()
         local vm = ply:GetViewModel()
         local holsterweapon = ply:GetWeapon(holster)
-        local based = IsValid(weapon) && !(weapon.ArcCW || weapon.ARC9 || weapon.ArcticTacRP || weapon.IsTFAWeapon || weapon.CW20Weapon || weapon.IsFAS2Weapon || weapon.IsUT99Weapon || weapons.IsBasedOn(weapon:GetClass(), "weapon_ss2_base") || weapons.IsBasedOn(weapon:GetClass(), "weapon_ut2004_base") || (weapons.IsBasedOn(weapon:GetClass(), "weapon_hlaz_base") && GetConVar("hlaz_sv_holster"):GetBool()) || (weapons.IsBasedOn(weapon:GetClass(), "weapon_ss_base") && GetConVar("ss_enableholsterdelay"):GetBool()))
+
+        HLAZCvar = HLAZCvar || GetConVar("hlaz_sv_holster")
+        SSCVar = SSCVar || GetConVar("ss_enableholsterdelay")
+
+        local based = IsValid(weapon) && !(weapon.ArcCW || weapon.ARC9 || weapon.ArcticTacRP || weapon.IsTFAWeapon || weapon.CW20Weapon || weapon.IsFAS2Weapon || weapon.IsUT99Weapon || weapons.IsBasedOn(weapon:GetClass(), "weapon_ss2_base") || weapons.IsBasedOn(weapon:GetClass(), "weapon_ut2004_base") || (weapons.IsBasedOn(weapon:GetClass(), "weapon_hlaz_base") && SSCVar:GetBool()) || (weapons.IsBasedOn(weapon:GetClass(), "weapon_ss_base") && SSCvar:GetBool()))
         local t = 0
+
         ply.Holstering = true
+
         if based then
             if vm:SelectWeightedSequence(ACT_VM_HOLSTER) != -1 then
                 t = (ply:Ping() * 0.001) + vm:SequenceDuration(vm:SelectWeightedSequence(ACT_VM_HOLSTER))
@@ -52,6 +62,7 @@ if CLIENT then
                 end
             end
         end
+
         timer.Simple(t, function()
             if weapon == holsterweapon && (ply:Alive() && IsValid(ply.HolsterWep)) then
                 input.SelectWeapon(ply.HolsterWep)
@@ -63,6 +74,7 @@ if CLIENT then
             end
             ply.Holstering = false
         end)
+
         net.Start("holstering", false)
         net.WriteFloat(t)
         net.SendToServer()
@@ -76,7 +88,7 @@ if CLIENT then
 
     hook.Add("Think", "HolsterThink", function()
         local ply = LocalPlayer()
-        if IsValid(ply) && (ply:Alive() && IsValid(ply:GetActiveWeapon()) && GetConVar("holsterweapon_ladders"):GetBool()) then
+        if IsValid(ply) && (ply:Alive() && IsValid(ply:GetActiveWeapon()) && LadderCVar:GetBool()) then
             local weapon = ply:GetActiveWeapon()
             local holstered = weapon:GetClass() == holster
             local based = weapons.IsBasedOn(weapon:GetClass(), "mg_base") || weapons.IsBasedOn(weapon:GetClass(), "kf_zed_pill")
@@ -90,7 +102,6 @@ if CLIENT then
             end
         end
     end)
-
 end
 
 if SERVER then
@@ -110,7 +121,7 @@ if SERVER then
             ply:Give(holster, true)
             timer.Simple(t, function() ply:SelectWeapon(holster) end)
         end
-        if GetConVar("holsterweapon_ladders"):GetInt() == 2 && ply:GetMoveType() == MOVETYPE_LADDER then
+        if LadderCVar:GetInt() == 2 && ply:GetMoveType() == MOVETYPE_LADDER then
             ply:SetActiveWeapon(NULL)
             ply:SelectWeapon(holster)
         elseif t != 0 then
@@ -135,7 +146,7 @@ end
 
 if game.SinglePlayer() || CLIENT then
     hook.Add("PlayerSwitchWeapon", "HolsterWeaponSwitchHook", function(ply, oldwep, newwep)
-        if IsValid(oldwep) && GetConVar("holsterweapon_ladders"):GetBool() && ply:GetMoveType() == MOVETYPE_LADDER && ply:GetActiveWeapon():GetClass() == holster then
+        if IsValid(oldwep) && LadderCVar:GetBool() && ply:GetMoveType() == MOVETYPE_LADDER && ply:GetActiveWeapon():GetClass() == holster then
             return true
         end
     end)
